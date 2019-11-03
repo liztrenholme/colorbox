0524e741af48da874c2dc1c72dc0db2a89a10dc8
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2019-11-03 00:19:26 -0400

    Updates

--- precache-manifest.c403103b11f3980b4a8a9da8cdecac7f.js
+++ precache-manifest.c403103b11f3980b4a8a9da8cdecac7f.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "25b34a6865d468fe33207a1283e9309b",
+    "revision": "b55ecdf3872e2675177a8f865ff0814a",
     "url": "/colorbox/index.html"
   },
   {
-    "revision": "b39705070c975c1e5c2e",
-    "url": "/colorbox/static/css/main.3837fcc9.chunk.css"
+    "revision": "c3111b8aca9aff6bc8af",
+    "url": "/colorbox/static/css/main.c16a70e3.chunk.css"
   },
   {
     "revision": "6c800b365be7270d9040",
     "url": "/colorbox/static/js/2.bc1ddb32.chunk.js"
   },
   {
-    "revision": "b39705070c975c1e5c2e",
-    "url": "/colorbox/static/js/main.6ead6309.chunk.js"
+    "revision": "c3111b8aca9aff6bc8af",
+    "url": "/colorbox/static/js/main.04b44c72.chunk.js"
   },
   {
     "revision": "9403f51c73262fc9e4f5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/colorbox/precache-manifest.ec8b3269db39c122a81b21fcf2a6f100.js"
+  "/colorbox/precache-manifest.c403103b11f3980b4a8a9da8cdecac7f.js"
 );
 
 self.addEventListener('message', (event) => {
