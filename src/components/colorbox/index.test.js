dc24c74d97df972bc84bf1399f29003b1d1cf593
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2020-09-10 22:51:50 -0400

    test for main div and h1 banner

--- src/components/colorbox/index.test.js
+++ src/components/colorbox/index.test.js
@@ -1,10 +1,30 @@
 /* eslint-disable no-undef */
 import Colorbox from './index';
+import { describe } from 'yargs';
+import React from 'react';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 jest.mock('./index.js');
+
+configure({adapter: new Adapter()});
 
 beforeEach(() => {
   // Clear all instances and calls to constructor and all methods:
   Colorbox.mockClear();
+});
+
+describe('<Colorbox />', function () {
+  is('should render', () => {
+    const wrapper = shallow(<Colorbox />);
+    expect(wrapper.find('div')).to.have.length(1);
+  });
+});
+
+describe('<Colorbox />', function () {
+  is('should have a header', () => {
+    const wrapper = shallow(<Colorbox />);
+    expect(wrapper.find('h1')).to.have.length(1);
+  });
 });
 
 test('mode is never undefined', () => {
